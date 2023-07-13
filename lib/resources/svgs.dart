30257d874eb5da3831185504bb02976a04ee7da0
Etornam <sunumacbright@gmail.com>
2023-07-13 13:45:05 +0000

    added images

---
+++ lib/resources/svgs.dart
@@ -0,0 +1,10 @@
+part of 'resources.dart';
+
+class Svgs {
+  Svgs._();
+
+  static const String add = 'assets/svgs/add.svg';
+  static const String network = 'assets/svgs/network.svg';
+  static const String setting = 'assets/svgs/setting.svg';
+  static const String user = 'assets/svgs/user.svg';
+}
