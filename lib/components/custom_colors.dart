8954596c21a9770b415a2629041ca334713ce7c1
Etornam <sunumacbright@gmail.com>
2023-07-13 13:48:14 +0000

    added custom colors

---
+++ lib/components/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TraidColor {
+  static Color irisAccent = const Color.fromRGBO(84, 84, 209, 1);
+  static Color lightPurple = const Color.fromRGBO(228, 228, 247, 1);
+  static Color greyScaleLight = const Color.fromRGBO(230, 230, 235, 1);
+  static Color greyScale = const Color.fromRGBO(131, 131, 138, 1);
+  static Color greenDark = const Color.fromRGBO(0, 107, 16, 1);
+}
